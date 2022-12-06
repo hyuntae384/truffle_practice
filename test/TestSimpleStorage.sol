7646c41fb42b9016eb3fb698aa3dd20ef3f5ff57
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2022-12-06 12:04:13 +0900

    truffle

---
+++ test/TestSimpleStorage.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.10;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Sample.sol";
+
+contract TestSimpleStorage {
+    function testSimpleStorage() public {
+            SimpleStorage ss = new SimpleStorage();
+
+            uint expected = 4;
+            ss.set(expected);
+            Assert.equal(ss.get(), expected, "value equal test");
+
+    }
+}
